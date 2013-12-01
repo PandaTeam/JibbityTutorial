611a1448a56739dc5fb470a750a59a2183e0146c
yksrep <yksrep@gmail.com>
2013-12-01 20:32:14 +1000

    Tutorial 5 finished

--- src/tutorial/items/TutItem.java
+++ src/tutorial/items/TutItem.java
@@ -2,7 +2,12 @@ package tutorial.items;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import tutorial.lib.ModInfo;
 import tutorial.lib.Names;
 import cpw.mods.fml.relauncher.Side;
@@ -18,6 +23,30 @@ public TutItem(int id) {
 @SideOnly(Side.CLIENT)
 public void registerIcons(IconRegister icon) {
 	itemIcon = icon.registerIcon(ModInfo.ID.toLowerCase() + ":" + "tutItem");
+
 	}
+
+@Override
+public boolean itemInteractionForEntity(ItemStack itemstack, EntityPlayer player, EntityLivingBase target){
+
+	if(!target.worldObj.isRemote){
+			if(target instanceof EntityCreeper || target instanceof EntityZombie){
+				target.motionY = 2;}
+	
+	} else {
+		player.addChatMessage("No Creepers or Zombies in range!");
+	}
+
+return false;
+
+ }
+
+
+
 }
+
+
+
+
+
 
